f33baf76dd2c3ede6545db536808e0d180709bac
Falco13 <keanu0608@gmail.com>
2024-07-18 15:50:13 +0300

    home page controller, route home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <br>
 <div class="container-fluid">
     <form method="GET" action="" class="d-flex">
-        <input class="form-control me-2" id="s" name="s" type="search" placeholder="Search by name">
+        <input class="form-control me-2" id="s" name="s" type="search" placeholder="Search">
         <button class="btn btn-warning" type="submit">Search</button>
     </form>
 </div>
@@ -12,12 +12,14 @@
 
 <div class="container">
     <div class="row">
+        @foreach ($posts as $post)
         <div class="col-6">
             <div class="card">
-                <div class="card_header"></div>
-                <div class="card_body"></div>
+                <div class="card_header">{{ $post->title }}</div>
+                <div class="card_body">{{ $post->description }}</div>
             </div>
         </div>
+        @endforeach
     </div>
 </div>
 @endsection

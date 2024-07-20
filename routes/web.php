eeaac65b89376e06d7a549ac1d96db18f70845c2
Falco13 <keanu0608@gmail.com>
2024-07-20 19:35:40 +0300

    auth

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('/searсh', [PostController::class, 'search'])->name('search');
@@ -11,3 +12,7 @@ Route::get('/show/{id}', [PostController::class, 'show'])->name('show');
 Route::get('/edit/{id}', [PostController::class, 'edit'])->name('edit');
 Route::patch('/update/{id}', [PostController::class, 'update'])->name('update');
 Route::delete('/{id}', [PostController::class, 'destroy'])->name('destroy');
+
+Auth::routes();
+
+Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
